8f51a65d3038dd4190789ea8287842fdeb2b5688
Rengamani3 <127839457+Rengamani3@users.noreply.github.com>
2023-05-26 13:50:55 +0530

    Update admin.py

--- Code_X_Mania/bot/plugins/admin.py
+++ Code_X_Mania/bot/plugins/admin.py
@@ -14,12 +14,13 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 broadcast_ids = {}
+from pyrogram.enums.parse_mode import ParseMode
 
 
 @StreamBot.on_message(filters.command("status") & filters.private & filters.user(Var.OWNER_ID))
 async def sts(c: Client, m: Message):
     total_users = await db.total_users_count()
-    await m.reply_text(text=f"**Numbers of users:** `{total_users}`", parse_mode="Markdown", quote=True)
+    await m.reply_text(text=f"**Numbers of users:** `{total_users}`", parse_mode=ParseMode.HTML, quote=True)
 
 
 @StreamBot.on_message(filters.command("broadcast") & filters.private & filters.user(Var.OWNER_ID) & filters.reply)
